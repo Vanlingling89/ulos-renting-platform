5c0eb9205872c3b629adb9b82a5f85e3232b229c
Vanlingling89 <tan.malvinryan@yahoo.com>
2021-12-16 15:11:51 +0800

    Update card_loading.common.js

--- src/resources/common/card_loading.common.js
+++ src/resources/common/card_loading.common.js
@@ -21,28 +21,28 @@ export default CardLoadingCommon;
 const DisplayCardLoadingCommon = () => {
   return (
     <React.Fragment>
-      <div className="bg-yellow rounded shadow-xl">
-        <div className="h-64 bg-gray-300 rounded-tr rounded-tl animate-pulse"></div>
+      <div className="bg-yellow-300 rounded shadow-xl">
+        <div className="h-64 bg-yellow-400 rounded-tr rounded-tl animate-pulse"></div>
 
         <div className="p-5">
-          <div className="h-8 rounded-sm bg-gray-300 animate-pulse mb-3"></div>
-          <div className="h-6 rounded-sm bg-gray-300 animate-pulse mb-4"></div>
+          <div className="h-8 rounded-sm bg-yellow-400 animate-pulse mb-3"></div>
+          <div className="h-6 rounded-sm bg-yellow-400 animate-pulse mb-4"></div>
 
           <div className="grid grid-cols-4 gap-1 mb-4">
-            <div className="col-span-3 h-6 rounded-sm bg-gray-300 animate-pulse"></div>
-            <div className="h-6 rounded-sm bg-gray-300 animate-pulse"></div>
+            <div className="col-span-3 h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
+            <div className="h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
 
-            <div className="col-span-2 h-6 rounded-sm bg-gray-300 animate-pulse"></div>
-            <div className="col-span-2 h-6 rounded-sm bg-gray-300 animate-pulse"></div>
+            <div className="col-span-2 h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
+            <div className="col-span-2 h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
 
-            <div className="h-6 rounded-sm bg-gray-300 animate-pulse"></div>
-            <div className="col-span-3 h-6 rounded-sm bg-gray-300 animate-pulse"></div>
-            <div className="col-span-2 h-6 rounded-sm bg-gray-300 animate-pulse"></div>
-            <div className="h-6 rounded-sm bg-gray-300 animate-pulse"></div>
+            <div className="h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
+            <div className="col-span-3 h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
+            <div className="col-span-2 h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
+            <div className="h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
           </div>
 
-          <div className="w-48 h-6 rounded-sm bg-gray-300 animate-pulse mb-1"></div>
-          <div className="w-40 h-6 rounded-sm bg-gray-300 animate-pulse"></div>
+          <div className="w-48 h-6 rounded-sm bg-yellow-400 animate-pulse mb-1"></div>
+          <div className="w-40 h-6 rounded-sm bg-yellow-400 animate-pulse"></div>
         </div>
       </div>
     </React.Fragment>
